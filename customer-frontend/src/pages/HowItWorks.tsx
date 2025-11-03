1c6fd99257ebe7f9fd5c9a51dd792d0c19dbdeca
SandyAbdullahi <ab2d222@gmail.com>
2025-11-03 14:34:08 +0300

    Lower logo and 'rudi' text by adding marginTop on Login, Register, and HowItWorks pages

--- customer-frontend/src/pages/HowItWorks.tsx
+++ customer-frontend/src/pages/HowItWorks.tsx
@@ -50,7 +50,7 @@ const HowItWorks = () => {
       >
         <Logo
           size="xl"
-          style={{ transform: 'scale(4.8)', transformOrigin: 'center', marginBottom: '2rem' }}
+          style={{ transform: 'scale(4.8)', transformOrigin: 'center', marginTop: '2rem', marginBottom: '2rem' }}
         />
         <h2
           style={{
